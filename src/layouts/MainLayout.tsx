bfb3c24b2c7b3c6169907941104db0a89b2230c3
afonso-adriano <adrianoafonso418@gmail.com>
2026-07-31 17:39:07 +0200

    Toda estrutura e base do projecto feita, agora vamos avançar nos outros processos de desenvolvimento do website

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+export default function MainLayout() {
+  return (
+    <>
+      {/* Navbar */}
+
+      <Outlet />
+
+      {/* Footer */}
+    </>
+  );
+}
